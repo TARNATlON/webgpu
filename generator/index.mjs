afb2700f2706975a03dc8e4e8a09c0058272a282
Jason Erb <jason.erb@sparist.com>
2022-04-07 13:35:12 -0400

    Remove unnecessary `async`

--- generator/index.mjs
+++ generator/index.mjs
@@ -53,7 +53,7 @@ function writeGeneratedFile(path, text, includeNotice = true) {
   }
 };
 
-async function generateBindings(version, enableMinification, includeMemoryLayouts) {
+function generateBindings(version, enableMinification, includeMemoryLayouts) {
   // copy dawn.json specification from dawn folder into into specification folder
   fs.copyFileSync(
     DAWN_PATH + "/dawn.json",
